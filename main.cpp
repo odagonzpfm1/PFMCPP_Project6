0715fdd051439f7575521bf5cf7e6ba55e3649d2
odagonzpfm1 <154253425+odagonzpfm1@users.noreply.github.com>
2023-12-28 14:08:24 +0000

    fixed formatting

--- main.cpp
+++ main.cpp
@@ -68,52 +68,55 @@ best you can.
 
 #include <iostream>
 #include <string>
-struct T {
-  int distance;
-  std::string name;
-  T(int v, const char *letterName) {
-    distance = v;             // Implicit "this"
-    this->name = *letterName; // Explicit "this"
-  }
-};
-
-struct A {
-  T *compare(T *a, T *b) 
-  {
-    if( a->distance < b->distance ) return a;
-    if( a->distance > b->distance ) return b;
-    return nullptr;
-  }
-};
-
-struct U {
-  float value1{0}, value2{0};
-  float updateValue(float *updatedValue)
-  {
-      this->value1 = *updatedValue;
-      while (std::abs(this->value2 - this->value1) > 0.001f) 
-      {
-        this->value2 += 0.01f;
-      }
-      return this->value2 * this->value1;
-  }
-};
-
-struct Z {
-  static float functionA (U *that, float *updatedValue) 
-  {
-    std::cout << "U's value1 value: " << that->value1 << std::endl;
-    that->value1 = *updatedValue;
-    std::cout << "U's value1 updated value: "
-              << that->value1 << std::endl;
-    while (std::abs(that->value2 - that->value1) > 0.001f) 
+struct T 
+{
+    int distance;
+    std::string name;
+    T(int v, const char *letterName) 
     {
-      that->value2 += 0.01f;
+        distance = v;             // Implicit "this"
+        this->name = *letterName; // Explicit "this"
     }
-    std::cout << "U's value2 updated value: "
-              << that->value2 << std::endl;
-    return that->value2 * that->value1;
-  }
+};
+
+struct A 
+{
+    T *compare(T *a, T *b) 
+    {
+        if (a->distance < b->distance) return a;
+        if (a->distance > b->distance) return b;
+        return nullptr;
+    }
+};
+
+struct U 
+{
+    float value1{0}, value2{0};
+    float updateValue(float *updatedValue)
+    {
+        this->value1 = *updatedValue;
+        while (std::abs(this->value2 - this->value1) > 0.001f) 
+        {
+            this->value2 += 0.01f;
+        }
+        return this->value2 * this->value1;
+    }
+};
+
+struct Z 
+{
+    static float functionA (U *that, float *updatedValue) 
+    {
+        std::cout << "U's value1 value: " << that->value1 << std::endl;
+        that->value1 = *updatedValue;
+        std::cout << "U's value1 updated value: " << that->value1 << std::endl;
+        while (std::abs(that->value2 - that->value1) > 0.001f) 
+        {
+            that->value2 += 0.01f;
+        }
+        std::cout << "U's value2 updated value: " << that->value2 << std::endl;
+        return that->value2 * that->value1;
+    }
 };
 
 /*
@@ -132,20 +135,21 @@ struct Z {
  Wait for my code review.
  */
 
-int main() {
-  T firstSign(11, "A");
-  T secondSign(17, "B");
+int main() 
+{
+    T firstSign(11, "A");
+    T secondSign(17, "B");
 
-  A f;
-  auto *smaller = f.compare(&firstSign, &secondSign);
-  std::cout << "the smaller one is << " << smaller->name << ".\nIf compare() returns a nullptr, then nothing will print out."<< std::endl;
+    A f;
+    auto *smaller = f.compare(&firstSign, &secondSign);
+    std::cout << "the smaller one is << " << smaller->name << ".\nIf compare() returns a nullptr, then nothing will print out."<< std::endl;
 
-  U firstU;
-  float updatedValue = 5.f;
-  std::cout << "[static func] firstU's multiplied values: \n" <<
-  Z::functionA(&firstU, &updatedValue) << std::endl; 
+    U firstU;
+    float updatedValue = 5.f;
+    std::cout << "[static func] firstU's multiplied values: \n" <<
+    Z::functionA(&firstU, &updatedValue) << std::endl; 
 
-  U secondU;
-  std::cout << "[member func] secondU's multiplied values: " <<
-  secondU.updateValue(&updatedValue) << std::endl;
+    U secondU;
+    std::cout << "[member func] secondU's multiplied values: " <<
+    secondU.updateValue(&updatedValue) << std::endl;
 }
